96aa6a8b539ebbe2a138e4ef3f097d64e6528a69
Smee18 <marcel.venturotti@icloud.com>
2025-08-06 15:53:28 +0300

    Fixed tests

--- aku_proto/backend/tests/test_main.py
+++ aku_proto/backend/tests/test_main.py
@@ -3,6 +3,11 @@ from backend.main import app
 
 client = TestClient(app)
 
-def test_read_root():
-    response = client.get("/")
+def test_process_data():
+    response = client.post("/process", json={
+        "valueA": 5,
+        "valueB": 3,
+        "valueC": 2
+    })
     assert response.status_code == 200
+    assert response.json() == {"result": 6} 
