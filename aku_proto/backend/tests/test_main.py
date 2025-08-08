47bd2bbc38e85ef36bd94c5710235835c3d6502a
Smee18 <marcel.venturotti@icloud.com>
2025-08-08 12:18:04 +0300

    Updated backend tests

--- aku_proto/backend/tests/test_main.py
+++ aku_proto/backend/tests/test_main.py
@@ -1,25 +1,29 @@
 from fastapi.testclient import TestClient
-from backend.main import app
+from main import app
 import os
 import pandas as pd
-from backend.main import compute_z_score
+from main import compute_z_score
 
+base_dir = os.path.dirname(os.path.abspath(__file__))
 client = TestClient(app)
 
 def test_file_exists_boy():
-    assert os.path.isfile("../../data/wfa_boys_0-to-5-years_zscores.csv")
+    file_path = os.path.join(base_dir, '../../data/wfa_boys_0-to-5-years_zscores.csv')
+    assert os.path.isfile(file_path)
 
 def test_can_load_file_boy():
-    df = pd.read_csv("../../data/wfa_boys_0-to-5-years_zscores.csv")
+    file_path = os.path.join(base_dir, '../../data/wfa_boys_0-to-5-years_zscores.csv')
+    df = pd.read_csv(file_path)
     assert not df.empty
 
 def test_file_exists_girl():
-    assert os.path.isfile("../../data/wfa_girls_0-to-5-years_zscores.csv")
+    file_path = os.path.join(base_dir, '../../data/wfa_girls_0-to-5-years_zscores.csv')
+    assert os.path.isfile(file_path)
 
 def test_can_load_file_girl():
-    df = pd.read_csv("../../data/wfa_girls_0-to-5-years_zscores.csv")
+    file_path = os.path.join(base_dir, '../../data/wfa_girls_0-to-5-years_zscores.csv')
+    df = pd.read_csv(file_path)
     assert not df.empty
-
 
 def test_compute_z_score():
     z = compute_z_score(10, 1.2, 12, 0.1)
